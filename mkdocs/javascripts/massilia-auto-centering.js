b5d103f79873a61ebdb4b0d5c70f0377d932ae87
rod2ik <rod2ik.dev@gmail.com>
2022-09-14 21:58:10 +0200

    maj

--- mkdocs/javascripts/massilia-auto-centering.js
+++ mkdocs/javascripts/massilia-auto-centering.js
@@ -1,33 +1,37 @@
 /* License: GNU GPLv3+, Rodrigo Schwencke (Copyleft) */
 
 window.addEventListener('load', function() {
-    // console.log("Then massilia-auto-centering PAGE IS LOADED");
+  console.log("massilia-auto-centering PAGE IS LOADED");
 
-    /* ====================================================================== */
-    /*                    Automatic Centering of Tables                       */
-    /* ====================================================================== */
-    document.querySelectorAll(".md-typeset table")
-    .forEach( table => { // Table defined in Markdown notation
-        if ((table.parentElement instanceof HTMLDivElement && table.parentElement.classList.contains("md-typeset__table")) 
-        && (table.parentElement.parentElement instanceof HTMLDivElement && table.parentElement.parentElement.classList.contains("md-typeset__scrollwrap"))) {
-            table.parentElement.parentElement.classList.add("center");
-        } else { // HTML Table tag in Markdown
-            table.classList.add("center");
-        }
-    });
-    
-    /* ====================================================================== */
-    /*                    Automatic Centering of iFrames                      */
-    /* ====================================================================== */
-    document.querySelectorAll(".md-content iframe")
-      .forEach( el => {
-      el.classList.add("center");
-    });
-    /* ====================================================================== */
-    /*                    Automatic Centering of videos                       */
-    /* ====================================================================== */
-    document.querySelectorAll(".md-content div.video-js")
-      .forEach( el => {
-      el.classList.add("center");
-    });
+  /* ====================================================================== */
+  /*                    Automatic Centering of Tables                       */
+  /* ====================================================================== */
+  document.querySelectorAll(".md-typeset table")
+  .forEach( table => { // Table defined in Markdown notation
+      if ((table.parentElement instanceof HTMLDivElement && table.parentElement.classList.contains("md-typeset__table")) 
+      && (table.parentElement.parentElement instanceof HTMLDivElement && table.parentElement.parentElement.classList.contains("md-typeset__scrollwrap"))) {
+          table.parentElement.parentElement.classList.add("center");
+      } else { // HTML Table tag in Markdown
+          table.classList.add("center");
+      }
+  });
+  
+  /* ====================================================================== */
+  /*                    Automatic Centering of iFrames                      */
+  /* ====================================================================== */
+  document.querySelectorAll(".md-content iframe")
+    .forEach( el => {
+    el.classList.add("center");
+  });
+  /* ====================================================================== */
+  /*                    Automatic Centering of videos                       */
+  /* ====================================================================== */
+  document.querySelectorAll(".md-content video")
+    .forEach( el => {
+    el.classList.add("center");
+  });
+  document.querySelectorAll(".md-content div.video-js")
+    .forEach( el => {
+    el.classList.add("center");
+  });
 });
