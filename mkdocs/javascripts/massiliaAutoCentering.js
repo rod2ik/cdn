44ffd744449a9ed67c9b6474de257d78b04b4f9b
Rod2ik <rod2ik.dev@gmail.com>
2024-10-17 21:03:18 +0200

    maj

--- mkdocs/javascripts/massiliaAutoCentering.js
+++ mkdocs/javascripts/massiliaAutoCentering.js
@@ -1,39 +1,59 @@
 /* License: GNU GPLv3+, Rodrigo Schwencke (Copyleft) */
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // console.log("massilia-auto-centering PAGE LOADED");
+
+  document.querySelectorAll(".md-content *")
+    .forEach(el => {
+      // Add 'center' class to element 'el' itself
+      if (el.tagName == "FIGURE" ||
+        el.tagName == "FIGCAPTION"
+      ) {
+        el.classList.add("center");
+      }
+      // Add 'center' class to parent element 'el'
+      Array.from(el.children).forEach(child => {
+        if (el.tagName != "ARTICLE" && el.tagName != "CENTER" &&
+          (child.tagName == "svg" || // ne pas mettre en majuscules
+            child.tagName == "FIGURE" ||
+            child.tagName == "FIGCAPTION")
+        ) {
+          el.classList.add("center");
+        }
+      })
+    });
 
   /* ====================================================================== */
   /*                    Automatic Centering of Tables                       */
   /* ====================================================================== */
   document.querySelectorAll(".md-typeset table")
-  .forEach( table => { // Table defined in Markdown notation
-      if ((table.parentElement instanceof HTMLDivElement && table.parentElement.classList.contains("md-typeset__table")) 
-      && (table.parentElement.parentElement instanceof HTMLDivElement && table.parentElement.parentElement.classList.contains("md-typeset__scrollwrap"))) {
-          table.parentElement.parentElement.classList.add("center");
+    .forEach(table => { // Table defined in Markdown notation
+      if ((table.parentElement instanceof HTMLDivElement && table.parentElement.classList.contains("md-typeset__table"))
+        && (table.parentElement.parentElement instanceof HTMLDivElement && table.parentElement.parentElement.classList.contains("md-typeset__scrollwrap"))) {
+        table.parentElement.parentElement.classList.add("center");
       } else if (!table.classList.contains("highlighttable") || !table.parentElement.classList.contains("highlight")) { // HTML Table tag in Markdown, but not code tables
-          table.classList.add("center");
+        table.classList.add("center");
       }
-  });
-  
+    });
+
   /* ====================================================================== */
   /*                    Automatic Centering of iFrames                      */
   /* ====================================================================== */
   document.querySelectorAll(".md-content iframe")
-    .forEach( el => {
-    el.classList.add("center");
-  });
+    .forEach(el => {
+      el.classList.add("center");
+    });
   /* ====================================================================== */
   /*                    Automatic Centering of videos                       */
   /* ====================================================================== */
   // Any video tag
   document.querySelectorAll(".md-content video")
-    .forEach( el => {
-    el.classList.add("center");
-  });
+    .forEach(el => {
+      el.classList.add("center");
+    });
   // Videos of the video-js library
   document.querySelectorAll(".md-content div.video-js")
-    .forEach( el => {
-    el.classList.add("center");
-  });
+    .forEach(el => {
+      el.classList.add("center");
+    });
 });
--- mkdocs/javascripts/massiliaImages.js
+++ mkdocs/javascripts/massiliaImages.js
@@ -1,48 +1,50 @@
 /* License: GNU GPLv3+, Rodrigo Schwencke (Copyleft) */
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     // console.log("massilia-images PAGE LOADED");
 
     /* ====================================================================== */
     /*                       Get Images                              */
     /* ====================================================================== */
-    document.querySelectorAll(".md-typeset p")
-    .forEach(p => {
-        Array.from(p.children).forEach( el => {
-            if (el.tagName == "IMG") {
-                let img = el;
-                // console.log("img=", img);
-                // get Infos
-                let imgWidthAttribute = img.getAttribute("width");
-                let altText = img.getAttribute("alt");
-                if (altText == null) {
-                    altText="";
-                }
-                let imgClassList = img.classList;
-                // let parentP = img.parentElement;
-                // let parentOfParentP = img.parentElement.parentElement;
-                let parentP = p;
-                let parentOfParentP = p.parentElement;
-        
-                if (!img.classList.contains("twemoji") && !img.classList.contains("emojione")) { //emojis are not treated
-                    // create HTML container Elements
-                    let figure = this.document.createElement("figure");
-                    let figcaption = this.document.createElement("figcaption");
-                    let altTextElement = this.document.createTextNode(altText);
-                    if (!imgClassList.length == 0) {
-                        imgClassList.forEach( classItem => {
-                            figure.classList.add(classItem);
-                        });
+    // document.querySelectorAll(".md-typeset p")
+    document.querySelectorAll(".md-content p")
+        .forEach(p => {
+            Array.from(p.children).forEach(el => {
+                if (el.tagName == "IMG") {
+                    let img = el;
+                    // get Infos
+                    let imgWidthAttribute = img.getAttribute("width");
+                    let altText = img.getAttribute("alt");
+                    if (altText == null) {
+                        altText = "";
                     }
-                    figure.style.width = imgWidthAttribute;
-                    img.style.width = "100%";
-                    figure.appendChild(img);
-                    figcaption.appendChild(altTextElement);
-                    figcaption.style.display = "block";
-                    figure.appendChild(figcaption);
-                    p.appendChild(figure);
-                } // End of Emojis not Treated
-            } // End of Image Elements
+                    let imgClassList = img.classList;
+                    // let parentP = img.parentElement;
+                    // let parentOfParentP = img.parentElement.parentElement;
+                    let parentP = p;
+                    let parentOfParentP = p.parentElement;
+
+                    if (!img.classList.contains("twemoji") && !img.classList.contains("emojione")) { //emojis are not treated
+                        // create HTML container Elements
+                        let figure = this.document.createElement("figure");
+                        figure.classList.add("center");
+                        let figcaption = this.document.createElement("figcaption");
+                        figcaption.classList.add("center");
+                        let altTextElement = this.document.createTextNode(altText);
+                        if (!imgClassList.length == 0) {
+                            imgClassList.forEach(classItem => {
+                                figure.classList.add(classItem);
+                            });
+                        }
+                        figure.style.width = imgWidthAttribute;
+                        img.style.width = "100%";
+                        figure.appendChild(img);
+                        figcaption.appendChild(altTextElement);
+                        figcaption.style.display = "block";
+                        figure.appendChild(figcaption);
+                        p.appendChild(figure);
+                    } // End of Emojis not Treated
+                } // End of Image Elements
             });
         });
-    })
+})
