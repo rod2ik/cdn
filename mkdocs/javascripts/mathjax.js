2edc602afc0175e93ae2c34ec2a8df2c315f64bc
rod2ik <rod2ik.dev@gmail.com>
2025-09-14 21:47:12 +0200

    maj

--- mkdocs/javascripts/mathjax.js
+++ mkdocs/javascripts/mathjax.js
@@ -30,8 +30,7 @@ window.MathJax = {
       ssi: '{\\Leftrightarrow}',
       sssi: '{\\Longleftrightarrow}',
       // arcs
-      arc: ['{\\overparen{#1}}', 1],
-      wideparen: ['{\\overparen{#1}}', 1],
+      arc: ['{\\displaystyle \\overparen\{#1\}}', 1],
       RR: '{\\bf R}',
       euro: ['{\\unicode \{0x20AC\}}'],
       bold: ['{\\bf #1}',1],
