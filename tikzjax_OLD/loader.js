d0917d473c0e5770f8f1982de124d8335cdfef01
rod2ik <rod2ik.dev@gmail.com>
2026-06-23 21:36:59 +0200

    Delete OLD repos

--- tikzjax_OLD/loader.js
+++
@@ -1,9 +0,0 @@
-async function loadAllTikzModules() {
-    // console.log("massilia-xtables PAGE LOADED");
-    
-    await import('./tikzjax.js');
-    await import('./tikzjax-dark.js');
-
-}
-
-loadAllTikzModules();
